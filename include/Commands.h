d74b89898acf795120a33eb67ed5fac0122e82f9
GeorgiyIshchenko <gogaigd2005@gmail.com>
2024-03-01 18:28:37 +0300

    declarated all commands

--- include/Commands.h
+++ include/Commands.h
@@ -1,44 +1,158 @@
+#include <ProcessorEmulator.h>
+
 #ifndef PROCESSOREMULATOR_COMMANDS_H
 #define PROCESSOREMULATOR_COMMANDS_H
 
 namespace processorEmulator {
 
-    class BaseCommand {
+    namespace Commands {
 
-    public:
+        using argType = int;
 
-        virtual double execute();
+        class BaseCommand {
 
-    };
+        public:
 
-//    class Begin: BaseCommand{
-//
-//        double execute() override;
-//
-//    };
-//
-//    class End: BaseCommand{
-//
-//        double execute() override;
-//
-//    };
-//
-//    class Push: BaseCommand{
-//
-//        explicit Push(double value): _value(value) {}
-//
-//        double execute() override;
-//
-//    private:
-//
-//        double _value;
-//
-//    };
-//
-//    class
-//
-//
-//
+            virtual argType execute() { return argType{}; };
+
+        };
+
+        class Begin : public BaseCommand {
+
+            argType execute() override;
+
+        };
+
+        class End : public BaseCommand {
+
+            argType execute() override;
+
+        };
+
+        class Push : public BaseCommand {
+
+        public:
+
+            explicit Push(argType value) : _value(value) {}
+
+            argType execute() override;
+
+        private:
+
+            argType _value;
+
+        };
+
+        class Pop : public BaseCommand {
+
+            argType execute() override;
+
+        };
+
+        class PushR : public BaseCommand {
+
+        public:
+
+            explicit PushR(Register reg) : _reg(reg) {};
+
+            argType execute() override;
+
+        private:
+
+            Register _reg;
+
+        };
+
+        class PopR : public BaseCommand {
+
+            argType execute() override;
+
+        };
+
+        class MathCommand : public BaseCommand {
+
+        public:
+
+            explicit MathCommand(argType arg1, argType arg2) : _arg1(arg1), _arg2(arg2) {};
+
+        private:
+
+            argType _arg1;
+            argType _arg2;
+
+        };
+
+        class Add : public MathCommand {
+
+        public:
+
+            Add(argType arg1, argType arg2) : MathCommand(arg1, arg2) {};
+
+            argType execute() override;
+
+        };
+
+        class Sub : public MathCommand {
+
+        public:
+
+            Sub(argType arg1, argType arg2) : MathCommand(arg1, arg2) {};
+
+            argType execute() override;
+
+        };
+
+        class Mul : public MathCommand {
+
+        public:
+
+            Mul(argType arg1, argType arg2) : MathCommand(arg1, arg2) {};
+
+            argType execute() override;
+
+        };
+
+        class Div : public MathCommand {
+
+        public:
+
+            Div(argType arg1, argType arg2) : MathCommand(arg1, arg2) {};
+
+            argType execute() override;
+
+        };
+
+        class In : public BaseCommand {
+
+        public:
+
+            explicit In(argType arg) : _arg(arg) {};
+
+            argType execute() override;
+
+        private:
+
+            argType _arg;
+
+        };
+
+        class Out : public BaseCommand {
+
+        public:
+
+            explicit Out(argType arg) : _arg(arg) {};
+
+            argType execute() override;
+
+        private:
+
+            argType _arg;
+
+        };
+
+
+    }
+
 }
 
 #endif
--- src/Commands.cpp
+++ src/Commands.cpp
@@ -4,7 +4,51 @@
 namespace processorEmulator {
 
 
-    double processorEmulator::BaseCommand::execute() {
-        return 0;
+    Commands::argType Commands::Begin::execute() {
+        return BaseCommand::execute();
+    }
+
+    Commands::argType Commands::End::execute() {
+        return BaseCommand::execute();
+    }
+
+    Commands::argType Commands::Push::execute() {
+        return BaseCommand::execute();
+    }
+
+    Commands::argType Commands::Pop::execute() {
+        return BaseCommand::execute();
+    }
+
+    Commands::argType Commands::PushR::execute() {
+        return BaseCommand::execute();
+    }
+
+    Commands::argType Commands::PopR::execute() {
+        return BaseCommand::execute();
+    }
+
+    Commands::argType Commands::Add::execute() {
+        return BaseCommand::execute();
+    }
+
+    Commands::argType Commands::Sub::execute() {
+        return BaseCommand::execute();
+    }
+
+    Commands::argType Commands::Mul::execute() {
+        return BaseCommand::execute();
+    }
+
+    Commands::argType Commands::Div::execute() {
+        return BaseCommand::execute();
+    }
+
+    Commands::argType Commands::In::execute() {
+        return BaseCommand::execute();
+    }
+
+    Commands::argType Commands::Out::execute() {
+        return BaseCommand::execute();
     }
 }
